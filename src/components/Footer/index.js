17cbfc4704153636904bf16833d5e62e36fdc5e4
salman1696 <33120544+salman1696@users.noreply.github.com>
2022-03-05 01:37:56 +0500

    buttonUpdated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 // import Particles from 'react-particles-js'
 
-import Bubble from '../../assets/images/bubble-lg.png'
-import './style.css'
+import Bubble from '../../assets/images/bubble-lg.png';
+import './style.css';
 
 const Footer = () => {
   return (
     <>
-      <div className="footer" id="contact">
+      <div className='footer' id='contact'>
         {/* <div className="particals">
           <Particles
             params={{
@@ -64,20 +64,20 @@ const Footer = () => {
             }}
           />
         </div> */}
-        <img src={Bubble} alt="bubble" className="left-bubble" />
-        <img src={Bubble} alt="bubble" className="right-bubble" />
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 col-md-6">
-              <div className="contact">
-                <div className="contat-title mb-5">CONTACT US</div>
-                <div className="contacts-info">
-                  Phone :<a href="/">(646) 586 3347</a>
+        <img src={Bubble} alt='bubble' className='left-bubble' />
+        <img src={Bubble} alt='bubble' className='right-bubble' />
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-6 col-md-6'>
+              <div className='contact'>
+                <div className='contat-title mb-5'>CONTACT US</div>
+                <div className='contacts-info'>
+                  Phone :<a href='/'>(646) 586 3347</a>
                 </div>
-                <div className="contacts-info">
-                  Email :<a href="/">info@getbubblesapp.com</a>
+                <div className='contacts-info'>
+                  Email :<a href='/'>info@getbubblesapp.com</a>
                 </div>
-                <div className="contacts-info">
+                <div className='contacts-info'>
                   <h2>Mailing Address:</h2>
                   <p>
                     1465 5th Avenue Apt 5c, <br /> New York, NY 10035
@@ -85,51 +85,61 @@ const Footer = () => {
                 </div>
               </div>
             </div>
-            <div className="col-lg-6 col-md-6">
-              <div className="contact">
-                <div className="contat-title">Feel free to contact us</div>
-                <div className="contat-des">
+            <div className='col-lg-6 col-md-6'>
+              <div className='contact'>
+                <div className='contat-title'>Feel free to contact us</div>
+                <div className='contat-des'>
                   If you are interested in finding out more about Bubbles.
                   <br /> If you would like to join our listing and benefit from
                   our services.
                 </div>
                 <form>
-                  <div className="form-group">
+                  <div className='form-group'>
                     <label>Name *</label>
-                    <input type="text" className="form-control" />
+                    <input type='text' className='form-control' />
                   </div>
-                  <div className="form-group">
+                  <div className='form-group'>
                     <label>Email *</label>
-                    <input type="email" className="form-control" />
+                    <input type='email' className='form-control' />
                   </div>
-                  <div className="form-group">
+                  <div className='form-group'>
                     <label>Message </label>
                     <textarea
-                      type="text"
-                      className="form-control"
+                      type='text'
+                      className='form-control'
                       rows={4}
                     ></textarea>
+                  </div>
+                  <div className='form-inline '>
+                    <a
+                      href=''
+                      className='btn btn-primary my-2 my-sm-0'
+                      type='button'
+                      target='blank'
+                    >
+                      Send
+                    </a>
                   </div>
                 </form>
               </div>
             </div>
           </div>
         </div>
-        <div className="bottom-footer">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-md-6">
+        <div className='bottom-footer'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-lg-6 col-md-6'>
                 2022 All &copy; Rights Reserved By Bubbles
               </div>
-              <div className="col-lg-6 col-md-6 text-right">
-                <Link to="/policy">Privacy & Terms of Services</Link>
+              <div className='col-lg-6 col-md-6 text-right'>
+                <Link to='/policy'>Privacy & Terms of Services</Link>
               </div>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
