d104591d03732c297926b7bb14ef77d4569927d6
Iota labs <Abc@gamil.com>
2022-03-10 20:33:31 +0500

    tempcode

--- src/components/Home/Steps/index.js
+++ src/components/Home/Steps/index.js
@@ -10,33 +10,33 @@ const Steps = () => {
   return (
     <>
       <div className="steps">
-        <div className="services-sec-title">HOW IT WORKS</div>
+        <div className="services-sec-title">WHAT WE DO</div>
         <img src={Bubble} alt="bubble" className="left-bubble" />
         <img src={Bubble} alt="bubble" className="right-bubble" />
         <div className="container">
           <div className="steps-item">
             <div className="row">
-              <div className="col-lg-6 col-md-6 justify-content-center d-flex flex-column">
+              <div className="col-lg-6 justify-content-center d-flex flex-column">
                 <div className="step-title">STEP 1</div>
                 <div className="step-des">
                   Choose the laundromat - dry cleaner of your choice
                 </div>
               </div>
-              <div className="col-lg-6 col-md-6">
+              <div className="col-lg-6">
                 <div className="step-img">
                   <img src={StepOne} alt="" />
                 </div>
               </div>
             </div>
           </div>
-          <div className="steps-item reverse">
+          <div className="steps-item">
             <div className="row">
-              <div className="col-lg-6 col-md-6">
+              <div className="col-lg-6">
                 <div className="step-img">
                   <img src={StepTwo} alt="" />
                 </div>
               </div>
-              <div className="col-lg-6 col-md-6 justify-content-center d-flex flex-column">
+              <div className="col-lg-6 justify-content-center d-flex flex-column">
                 <div className="step-title">STEP 2</div>
                 <div className="step-des">
                   Schedule a pick up and delivery date
@@ -46,13 +46,13 @@ const Steps = () => {
           </div>
           <div className="steps-item mb-0">
             <div className="row">
-              <div className="col-lg-6 col-md-6 justify-content-center d-flex flex-column">
+              <div className="col-lg-6 justify-content-center d-flex flex-column">
                 <div className="step-title">STEP 3</div>
                 <div className="step-des">
                   Sit back and receive on time clean laundry
                 </div>
               </div>
-              <div className="col-lg-6 col-md-6">
+              <div className="col-lg-6">
                 <div className="step-img">
                   <img src={StepThree} alt="" />
                 </div>
